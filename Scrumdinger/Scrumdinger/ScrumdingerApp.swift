18869115fc556da4ef27de212434e16e218c8198
FreddyQuispe <fquispe@smartsolutions.com.pe>
2021-02-08 00:12:55 -0500

    SwiftUI & Code Essentials & Card View

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Freddy Alexander Quispe Torres on 7/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
